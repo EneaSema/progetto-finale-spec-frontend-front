7c5dc91c5baebb8bd595e12eb35149518b5aa922
EneaSema <sema.enea1993@gmail.com>
2025-10-29 19:10:56 +0100

    finish  final project

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 // 1. Creazione del Context
 const ProductContext = createContext();
@@ -10,12 +10,28 @@ export const useProducts = () => {
 
 // 3. Provider: Gestisce lo stato e la logica
 export const ProductProvider = ({ children }) => {
-  const [favoriteIds, setFavoriteIds] = useState([]);
+  // Legge i Preferiti (o usa array vuoto se non trova nulla)
+  const initialFavoriteIds =
+    JSON.parse(localStorage.getItem("favoriteIds")) || [];
+  const [favoriteIds, setFavoriteIds] = useState(initialFavoriteIds);
 
-  // Stato Confronto
-  const [compareIds, setCompareIds] = useState([]);
-
+  // Legge la lista Confronto (o usa array vuoto se non trova nulla)
+  const initialCompareIds =
+    JSON.parse(localStorage.getItem("compareIds")) || [];
+  const [compareIds, setCompareIds] = useState(initialCompareIds);
   console.log("ID Prodotti da Confrontare:", compareIds);
+
+  // Effetto per salvare i Preferiti
+  useEffect(() => {
+    // Salva l'array di ID in localStorage, convertito in stringa
+    localStorage.setItem("favoriteIds", JSON.stringify(favoriteIds));
+  }, [favoriteIds]); // Si attiva ogni volta che favoriteIds cambia
+
+  // Effetto per salvare i Confronti
+  useEffect(() => {
+    // Salva l'array di ID in localStorage, convertito in stringa
+    localStorage.setItem("compareIds", JSON.stringify(compareIds));
+  }, [compareIds]); // Si attiva ogni volta che compareIds cambia
 
   // Funzione di Confronto: Aggiunge o Rimuove un ID dalla lista di confronto
   const toggleCompare = (productId) => {
